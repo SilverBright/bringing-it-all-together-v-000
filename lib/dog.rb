98512fdabb4efa6a4665d28b545cf7fdf5ca89ab
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 18:40:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,26 +27,17 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)
       SQL
-
+  
       DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
   end
-
-  # def save
-  #   if self.id
-  #     self.update
-  #   else
-  #     sql = <<-SQL
-  #     INSERT INTO dogs (name, breed)
-  #     VALUES (?, ?)
-  #     SQL
-  #
-  #     DB[:conn].execute(sql, self.name, self.breed)
-  #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-  #   end
-  # end
 
 end
