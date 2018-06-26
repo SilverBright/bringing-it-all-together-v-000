81852db2fc217c3c45707af784029de5fa66e79a
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-26 18:40:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,10 +34,13 @@ class Dog
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)
       SQL
-  
+
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
+  end
+
+  def self.create_table
   end
 
 end
